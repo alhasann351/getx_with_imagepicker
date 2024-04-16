dcdce181cf9bab5d8a7fee7283c4f0891c79b882
alhasann351 <alhasann352@gmail.com>
2024-04-16 22:01:54 +0600

    initial-commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.blue,
+      title: const Text(
+        'GetX ImagePicker',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22,
+          color: Colors.white,
+        ),
+      ),
+    ),);
+  }
+}
